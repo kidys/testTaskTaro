d451202b0c6ccdf84b6aa3d990d32e99847034fe
kidys <arsenal.fmfan@gmail.com>
2018-12-16 00:41:49 +0300

    Finish test Task Russian School Taro

--- app/index.js
+++ app/index.js
@@ -1,10 +1,16 @@
-/**
- * Application entry point
- */
-
-// Load application styles
 import 'styles/index.scss';
 
-// ================================
-// START YOUR APP HERE
-// ================================
+document.querySelectorAll('a').forEach((e) => {
+    e.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(e.currentTarget);
+    });
+});
+
+function handleHeaderBtn(e) {
+    e.preventDefault();
+    document.querySelector('.menuListWrapper_header').classList.toggle('menuListWrapper_header-mobileOpen');
+};
+
+document.querySelector('#headerMenuMobileBtnOpen').addEventListener('click', handleHeaderBtn);
+document.querySelector('#headerMenuMobileBtnClose').addEventListener('click', handleHeaderBtn);
